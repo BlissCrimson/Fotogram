c39484fe272afb71018fdd47102f277b9d5b350a
Matthias Tausch <m.tausch89@gmail.com>
2025-08-12 18:53:41 +0200

    start project Fotogram added: index.html; style.css; script.js; images

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = [
+    "img/aussichtlaboe.jpg",
+    "img/crimsonclouds.jpg",
+    "img/harz.jpg",
+    "img/sirbenjaminleeguiness.jpg",
+    "img/limmerbeinacht.jpg"
+];
+
+
+// DIALOG
+
+
+
+
+// OVERLAY
+
+function toggleOverlay(){
+    let overlayRef = document.getElementById('')
+
+    overlayRef.classList.remove('d_none')
+}
