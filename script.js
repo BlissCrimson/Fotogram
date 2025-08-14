fa435e452b37128d6f7832d410f6b73db7e56cc8
Matthias Tausch <m.tausch89@gmail.com>
2025-08-14 16:10:09 +0200

    feat: index; script; style

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const images = [
-    "img/aussichtlaboe.jpg",
-    "img/crimsonclouds.jpg",
-    "img/harz.jpg",
-    "img/sirbenjaminleeguiness.jpg",
-    "img/limmerbeinacht.jpg"
-];
+const myImages = [
+    "aussichtlaboe.jpg",
+    "crimsonclouds.jpg",
+    "harz.jpg",
+    "limmerbeinacht.jpg",
+    "sirbenjaminleeguiness.jpg"];
+
+const imagesNames = [
+    "Aussicht von La Boe",
+    "Wolken bei Sonnenuntergang",
+    "Aussicht im Harz",
+    "Hannover von der alten Conti bei Nacht",
+    "Sir Benjamin Lee Guiness"
+]
 
 
 // DIALOG
 
+let dialogRef = document.getElementById("imageDialog");
 
+function openDialog() {
+    dialogRef.showModal();
+}
+
+function closeDialog() {
+    dialogRef.close();
+}
 
 
 // OVERLAY
 
-function toggleOverlay(){
+function toggleOverlay() {
     let overlayRef = document.getElementById('')
 
     overlayRef.classList.remove('d_none')
