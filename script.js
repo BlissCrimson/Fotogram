f7d5de2b23d858cd196a26a3a7cfc68633712317
Matthias Tausch <m.tausch89@gmail.com>
2025-08-27 11:58:49 +0200

    perf: code shortened

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let myImages = [
-    "aussichtlaboe.jpg",
-    "crimsonclouds.jpg",
-    "harz.jpg",
-    "limmerbeinacht.jpg",
-    "sirbenjaminleeguiness.jpg"
+const myImages = [
+    "./img/aussichtlaboe.jpg",
+    "./img/crimsonclouds.jpg",
+    "./img/harz.jpg",
+    "./img/husumerhafen.jpg",
+    "./img/limmerbeinacht.jpg",
+    "./img/polarlichterinnorddeutschland.jpg",
+    "./img/sirbenjaminleeguiness.jpg",
+    "./img/sternenhimmelübernordfriesland.jpg",
 ]
 
-let imagesNames = [
+const imagesNames = [
     "Aussicht von La Boe",
     "Wolken bei Sonnenuntergang",
     "Aussicht im Harz",
+    "Husumer Hafen bei Nacht",
     "Hannover von der alten Conti bei Nacht",
-    "Sir Benjamin Lee Guiness"
+    "Polarlichter über Norddeutschland",
+    "Sir Benjamin Lee Guiness",
+    "Sternenhimmel über Nordfriesland"
 ]
 
-// let myObjectImage = {
-//     img1: "Aussicht von La Boe",
-//     img2: "Wolken bei Sonnenuntergang",
-//     img3: "Aussicht im Harz",
-//     img4: "Hannover von der alten Conti bei Nacht",
-//     img5: "Sir Benjamin Lee Guiness"
-// }
 
 // DIALOG
 
+// DIALOG HEADER
+
+// DIALOG OPEN/CLOSE
 let dialogRef = document.getElementById("imageDialog");
 // to open dialog
 function openDialog() {
     let overlayRef = document.getElementById('overlay');
     dialogRef.showModal();
     // add overlay behind dialog
+    // dialog.innerHTML = <img src="${myImages[index]}">
     overlayRef.classList.remove('d_none');
 }
+
 // to close dialog
 function closeDialog() {
     let overlayRef = document.getElementById('overlay');
@@ -40,57 +44,62 @@ function closeDialog() {
     overlayRef.classList.add('d_none');
 }
 
-// DIALOG RENDERING
 
+// MAIN RENDERING
 
-// let titleRef = document.getElementById("dialogTitle");
+function initRendering() {
+    renderTitle();
+    renderImage();
+}
 
-function renderDialog(myImages, imagesNames) {
-    let dialogImageRef = document.getElementById('dialogTitle');
-    for (let index = 0; index < myImages.length; index++) {
-        dialogImageRef.innerHTML += getNoteTemplate(index);
+// MAIN IMG RENDERING
+let imageRef = document.getElementById('main_images');
+
+function renderImage() {
+    imageRef.innerHTML = "";
+    for (let index = 0; index < myImages.length; index == index++) {
+        imageRef.innerHTML += `<img class="img_little" aria-haspopup="dialog" aria-controls="imageDialog" onclick="openDialog()" src="${myImages[index]}">`
     }
 }
 
-function getNoteTemplate(index) {
-    return document.getElementById('dialogTitle').innerHTML = `<h2>
-                                                                ${imagesNames[index]}
-                                                              </h2>`
-}
 
+// DIALOG
+
+// DIALOG RENDERING
+
+function renderDialog() {
+    renderTitle();
+    showImage();
+}
 
 // DIALOG TITLE
 
 let titleRef = document.getElementById('dialogTitle');
 
 function renderTitle() {
-    let temp
-    temp = document.getElementsByTagName("template");
-
+    titleRef.innerHTML = "";
+    for (let index = 0; index < imagesNames.length; index == index++) {
+        titleRef.innerHTML += `${imagesNames[index]}`
+        
+    }
 
 }
-
-
-
 
 
 // DIALOG IMAGE
 
-let imageRef = document.getElementById('dialogImage');
-
-function renderImage(params) {
-    let temp // for template
-    temp = document.getElementsByTagName("template");
-
-
-}
-
 function showImage() {
-    if (document.createElement("tempalte").content) {
-        imageRef = document.getElementById('dialogImage').innerHTML;
-    } else {
-        imageRef = document.getElementById('img1');
+    
+    for (let index = 0; index < myImages.length; index++) {
+        imageRef.innerHTML = `${myImages[index]}`
+        return imageRef.innerHTML = `<img class="dialog_image" onclick="openDialog(${index})" src="${myImages[index]}">`
     }
+    
+    // if (document.createElement("template").content) {
+    //     imageRef = document.getElementById('dialogImage').innerHTML;
+    // } else {
+    //     imageRef = document.getElementById('img1');
+    // }
 
 }
 
@@ -101,12 +110,9 @@ document.getElementById("arrowLeft").addEventListener("click", back);
 document.getElementById("arrowRight").addEventListener("click", foward);
 
 function back() {
-
     console.log("du drückst zurück");
-
 }
 
 function foward() {
     console.log("du drückst weiter");
-
 }
